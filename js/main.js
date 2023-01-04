4564017bcec2d55d58da331ae1fdf8690b660c91
Jose Alberto <80777765+JoseAlbertoRodrigues@users.noreply.github.com>
2023-01-04 12:26:59 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-    element.addEventListener('click', function () {
+    element.addEventListener('click', function() {
         nav.classList.toggle('show')
     })
 }
